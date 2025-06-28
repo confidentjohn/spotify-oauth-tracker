671271cb937fb2f11863876ea14c1223c3f43b5a
John Mitchell <johnnydmitch@gmail.com>
2025-06-27 23:01:49 -0400

    fix

--- utils/spotify_auth.py
+++ utils/spotify_auth.py
@@ -31,3 +31,16 @@ def get_access_token_for_user(user_id):
 def get_spotify_client_for_user(user_id):
     token = get_access_token_for_user(user_id)
     return Spotify(auth=token)
+
+def get_spotify_client():
+    token_response = requests.post(
+        "https://accounts.spotify.com/api/token",
+        data={
+            "grant_type": "refresh_token",
+            "refresh_token": os.environ["SPOTIFY_REFRESH_TOKEN"],
+            "client_id": os.environ["SPOTIFY_CLIENT_ID"],
+            "client_secret": os.environ["SPOTIFY_CLIENT_SECRET"],
+        }
+    )
+    token_response.raise_for_status()
+    return Spotify(auth=token_response.json()["access_token"])
